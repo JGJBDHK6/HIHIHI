be0821feaac624d6a0c83fa41197098bb2f10651
JIHYE <butter1398@inha.edu>
2022-09-12 17:56:48 +0900

    Create TypeInference.java
    02 변수와 타입 코딩

---
+++ JIHYE/02변수와타입/TypeInference.java
@@ -0,0 +1,17 @@
+package _02변수와타입;
+
+public class TypeInference {
+	   public static void main(String[] args) {
+	        int i = 10;
+	        var j = 10.0;
+	        var str = "hello";
+
+	        System.out.println(i);
+	        System.out.println(j);
+	        System.out.println(str);
+
+	        str = "test";
+
+	        //System.out.println(str);
+	   }
+	}
