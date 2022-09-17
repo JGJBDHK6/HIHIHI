5addd7b8c964089f717e212bcf03c577d13454c7
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-09-18 01:46:38 +0900

    Create HIHIHI_JAVASTUDY06.java

---
+++ Seolhwa/HIHIHI_JAVASTUDY06.java
@@ -0,0 +1,16 @@
+
+public class HIHIHI_JAVASTUDY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 65;
+		int b = -66;
+
+		char a2 = 65;
+
+		System.out.println((char) a);
+		System.out.println((char) b);
+		System.out.println((char) a2);
+	}
+
+}
