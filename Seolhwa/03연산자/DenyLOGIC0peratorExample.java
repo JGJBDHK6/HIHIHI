beb56093d78cac3a7af56a1557e64c4034623c08
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-09-18 02:01:01 +0900

    Create DenyLOGIC0peratorExample.java

---
+++ Seolhwa/03연산자/DenyLOGIC0peratorExample.java
@@ -0,0 +1,17 @@
+
+public class DenyLOGIC0peratorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 이것이 자바다 73번 예제
+		boolean play = true;
+		System.out.println(play);
+
+		play = !play;
+		System.out.println(play);
+
+		play = !play;
+		System.out.println(play);
+	}
+
+}
