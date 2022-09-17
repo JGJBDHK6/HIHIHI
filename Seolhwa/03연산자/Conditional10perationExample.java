7ba4edd39292540f7fa2211088184e876584f18e
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-09-18 03:04:18 +0900

    Create Conditional10perationExample.java

---
+++ Seolhwa/03연산자/Conditional10perationExample.java
@@ -0,0 +1,13 @@
+
+public class Conditional10perationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 이것이 자바다 101p 예정
+		int score = 85;
+		char grade = (score > 90) ? 'A' : ((score > 80) ? 'B' : 'C');
+		System.out.println(score + "점은" + grade + "등급입니다.");
+
+	}
+
+}
