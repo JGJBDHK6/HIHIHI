15e01f0133eb46e4d6f591e15fa30db3ef50e55a
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-09-18 01:46:36 +0900

    Create HIHIHI_JAVASTUDY04.java

---
+++ Seolhwa/HIHIHI_JAVASTUDY04.java
@@ -0,0 +1,13 @@
+
+public class HIHIHI_JAVASTUDY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch1 = 'วั';
+        char ch2 = '\uD55c';
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+	}
+
+}
