4ca6ea6a79f3170ebc099cbfcacfd0685f71b4de
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-09-18 22:04:47 +0900

    Create IfExample.java

---
+++ Seolhwa/04조건문과반복문/IfExample.java
@@ -0,0 +1,18 @@
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 이것이자바다 109페이지 예제
+		int score = 93;
+		if (score >= 90) {
+			System.out.println("점수가 90보다 큽니다");
+			System.out.println("등급은 A입니다.");
+		}
+		if (score < 90)
+			System.out.println("점수가 90보다 작습니다");
+		System.out.println("등급은 B입니다");
+
+	}
+
+}
