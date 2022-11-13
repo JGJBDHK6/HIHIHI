61a875695d5ccecf2ed6e1689006beea82019ebb
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-11-13 13:36:21 +0900

    Create _09짝수의합.java

---
+++ Seolhwa/Programmers/_09짝수의합.java
@@ -0,0 +1,16 @@
+public class _09짝수의합 {
+
+//class Solution {
+//    public int solution(int n) {
+//        int x = 0;
+//        for(;n>0; --n){
+//            if(n%2==0){
+//                x=x+n;
+//            }
+//        }
+//        return x;
+//    }
+//}
+
+
+}
