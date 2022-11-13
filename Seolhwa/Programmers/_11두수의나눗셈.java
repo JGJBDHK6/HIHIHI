1192ae2094bf35c840bb118c1164b4a96b0405d5
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-11-13 13:36:25 +0900

    Create _11두수의나눗셈.java

---
+++ Seolhwa/Programmers/_11두수의나눗셈.java
@@ -0,0 +1,21 @@
+public class _11두수의나눗셈 {
+    public static int main(String[] args) {
+        int num1 = 3;
+        int num2 = 2;
+
+        double x =(double)num1/num2*1000;
+        return (int)x;
+
+    }
+}
+
+
+//class Solution {
+//    public int solution(int num1, int num2) {
+//        double x =(double)num1/num2*1000;
+//        return (int)x;
+//    }
+//}
+
+// 인수로 받는 num1,2가 int인 점을 파악하지 못했다!
+
