5b6f2b0c40a8d5219251506dd0468696c1585162
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-11-13 13:36:27 +0900

    Create _12배열두배만들기.java

---
+++ Seolhwa/Programmers/_12배열두배만들기.java
@@ -0,0 +1,10 @@
+public class _12배열두배만들기 {
+    //class Solution {
+//    public int[] solution(int[] numbers) {
+//        for(int i = 0; i<numbers.length ; i++){
+//            numbers[i] = numbers[i]*2;
+//        }
+//        return numbers;
+//    }
+//}
+}
