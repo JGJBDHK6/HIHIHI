e0c2c9782452de27b7fa48c49fcc782bc4bc5c3c
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-11-13 13:36:46 +0900

    Create _23최댓값구하기_1.java

---
+++ Seolhwa/Programmers/_23최댓값구하기_1.java
@@ -0,0 +1,20 @@
+public class _23최댓값구하기_1 {
+}
+//class Solution {
+//    public int solution(int[] numbers) {
+//        for(int n = 0,i=1; n<numbers.length; ){
+//            for(; i<numbers.length;){
+//                if(numbers[n]<numbers[i]){
+//                    int x = numbers[n];
+//                    int h = numbers[i];
+//                    numbers[n] = h;
+//                    numbers[i] = x;
+//                }
+//                i++;
+//            }
+//            n++;
+//            i=n+1;
+//        }
+//        return numbers[0]*numbers[1];
+//    }
+//}
