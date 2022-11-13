e563495f2edf2cbd6ea580ccad4c08b97264a675
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-11-13 13:36:49 +0900

    Create _25배열자르기.java

---
+++ Seolhwa/Programmers/_25배열자르기.java
@@ -0,0 +1,11 @@
+public class _25배열자르기 {
+}
+//class Solution {
+//    public int[] solution(int[] numbers, int num1, int num2) {
+//       int[] answer = new int[(num2-num1)+1];
+//       for(int i=0; num1<num2+1; i++,num1++){
+//           answer[i] = numbers[num1];
+//       }
+//        return answer;
+//    }
+//}
