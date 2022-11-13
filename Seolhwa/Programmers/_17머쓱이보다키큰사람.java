d71dc7d6258e0f70c4c73115911c04777a61959a
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-11-13 13:36:37 +0900

    Create _17머쓱이보다키큰사람.java

---
+++ Seolhwa/Programmers/_17머쓱이보다키큰사람.java
@@ -0,0 +1,14 @@
+public class _17머쓱이보다키큰사람 {
+    //class Solution {
+//    public int solution(int[] array, int height) {
+//        int higher = 0;
+//        for(int a=0; a<array.length; a++){
+//            if(height<array[a]){
+//                higher+=1;
+//            }
+//        }
+//
+//        return higher;
+//    }
+//}
+}
