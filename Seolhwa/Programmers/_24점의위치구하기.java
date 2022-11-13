5ab6071fb9cb6f558cb93420646503f192f031e2
Seolhwaim <108778716+Seolhwaim@users.noreply.github.com>
2022-11-13 13:36:48 +0900

    Create _24점의위치구하기.java

---
+++ Seolhwa/Programmers/_24점의위치구하기.java
@@ -0,0 +1,21 @@
+public class _24점의위치구하기 {
+}
+//class Solution {
+//    public int solution(int[] dot) {
+//        if(dot[0]>0){
+//            if(dot[1]>0){
+//                return 1;
+//            }else{
+//                return 4;
+//            }
+//        }else{
+//            if(dot[1]>0){
+//                return 2;
+//            }else{
+//                return 3;
+//            }
+//        }
+//
+//
+//    }
+//}
